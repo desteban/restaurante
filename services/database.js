5d3d0b7dcd059cc304cac55a2e538f387c6cf5d4
desteban <desteban28c@hotmail.com>
2021-05-19 09:39:00 -0500

    credenciales de la variable de entorno

--- services/database.js
+++ services/database.js
@@ -3,19 +3,9 @@ import mysql from 'serverless-mysql';
 export let db = mysql({
 	config: {
 		host: process.env.HOSTNAME,
-		user: 'root',
+		user: process.env.USERDB,
 		password: process.env.PASSWORD,
 		database: process.env.DATABASE,
 		port: process.env.PORT
 	}
 });
-
-// import mysql from "serverless-mysql";
-
-// export let connection = mysql.createConnection({
-// host: process.env.HOSTNAME,
-// user: process.env.USERDB,
-// password: process.env.PASSWORD,
-// database: process.env.DATABASE,
-// port: process.env.PORT
-// });
